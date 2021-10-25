ea97d90c98c17c602f5c9b3d8c32c60d25b3c3ea
Shunro Dozono <dozono@cba-japan.com>
2021-10-25 17:46:03 +0900

    C# example

---
+++ c#_example/Dialler.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using FCSDKCLR;
+
+namespace WebRTCClientWinManaged
+{
+    /// <summary>
+    /// The interface that represents the available interactions from the
+    /// dialler controller.
+    /// </summary>
+    public interface IDiallerView
+    {
+        void AddLogMessage(string logMessage);
+        void SetController(DiallerController dc);
+        void SetLoggedIn(bool loggedIn);
+        void OnRemoteHangupCall();
+        /* A call has just become active */
+        void SetCallActive();
+        /* A call has just become inactive */
+        void SetCallInactive();
+        /* The phone has just started ringing */
+        void SetPhoneRinging();
+        /* Allow the hangup function to be activated */
+        void EnableHangupCapability();
+        /* Set the version information so that it can be displayed. */
+        void SetSDKVersion(String sdkVersion);
+    }
+
+    public partial class Dialler : Form, IDiallerView
+    {
+        private IDiallerWindowListener mController;
+
+        public Dialler()
+        {
+            InitializeComponent();
+            // The code below is to make sure items word wrap if they are too big to
+            // fit into the list box window.
+            // Ref: http://stackoverflow.com/questions/17613613/are-there-anyway-to-make-listbox-items-to-word-wrap-if-string-width-is-higher-th
+            listBoxActivity.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            listBoxActivity.MeasureItem += ListBoxMeasureItem;
+            listBoxActivity.DrawItem += ListBoxDrawItem;
+        }
+
+        // The code below is to make sure items word wrap if they are too big to
+        // fit into the list box window.
+        private void ListBoxMeasureItem(object sender, MeasureItemEventArgs e)
+        {
+            e.ItemHeight = (int)e.Graphics.MeasureString(
+                listBoxActivity.Items[e.Index].ToString(), 
+                listBoxActivity.Font, listBoxActivity.Width).Height;
+        }
+
+        // The code below is to make sure items word wrap if they are too big to
+        // fit into the list box window.
+        private void ListBoxDrawItem(object sender, DrawItemEventArgs e)
+        {
+            e.DrawBackground();
+            e.DrawFocusRectangle();
+            if (e.Index >= 0 && e.Index < listBoxActivity.Items.Count)
+            {
+                e.Graphics.DrawString(listBoxActivity.Items[e.Index].ToString(),
+                    e.Font, new SolidBrush(e.ForeColor), e.Bounds);
+            }
+        }
+        
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            mController.OnMenuEventLogin();
+        }
+
+        private void buttonLogout_Click(object sender, EventArgs e)
+        {
+            mController.OnMenuEventLogout();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxPhoneNum.Text += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBoxPhoneNum.Text += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBoxPhoneNum.Text += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBoxPhoneNum.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBoxPhoneNum.Text += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBoxPhoneNum.Text += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBoxPhoneNum.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBoxPhoneNum.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBoxPhoneNum.Text += "9";
+        }
+
+        private void buttonStar_Click(object sender, EventArgs e)
+        {
+            textBoxPhoneNum.Text += "*";
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            textBoxPhoneNum.Text += "0";
+        }
+
+        private void buttonHash_Click(object sender, EventArgs e)
+        {
+            textBoxPhoneNum.Text += "#";
+        }
+
+        private void buttonBackspace_Click(object sender, EventArgs e)
+        {
+            if (textBoxPhoneNum.Text.Length > 0)
+            {
+                string num = textBoxPhoneNum.Text;
+                num = num.Substring(0, num.Length - 1);
+                textBoxPhoneNum.Text = num;
+            }
+        }
+
+        private void buttonAnswer_Click(object sender, EventArgs e)
+        {
+            mController.OnAnswerCall(this.Bounds, audioDirection(), videoDirection());
+        }
+
+        private void buttonReject_Click(object sender, EventArgs e)
+        {
+            mController.OnRejectCall();
+        }
+
+        static readonly Dictionary<Boolean, Dictionary<Boolean, CLI_MediaDirection>> mMediaDirection;
+
+        static Dialler()
+        {
+            Dictionary<Boolean, CLI_MediaDirection> sendTrue = new Dictionary<Boolean, CLI_MediaDirection>();
+            sendTrue[true] = CLI_MediaDirection.SEND_AND_RECEIVE;
+            sendTrue[false] = CLI_MediaDirection.SEND_ONLY;
+            Dictionary<Boolean, CLI_MediaDirection> sendFalse = new Dictionary<Boolean, CLI_MediaDirection>();
+            sendFalse[true] = CLI_MediaDirection.RECEIVE_ONLY;
+            sendFalse[false] = CLI_MediaDirection.NONE;
+            mMediaDirection = new Dictionary<Boolean, Dictionary<Boolean, CLI_MediaDirection>>();
+            mMediaDirection[true] = sendTrue;
+            mMediaDirection[false] = sendFalse;
+        }
+
+        private CLI_MediaDirection audioDirection()
+        {
+            return mMediaDirection[checkBoxSendAudio.Checked][checkBoxReceiveAudio.Checked];
+        }
+
+        private CLI_MediaDirection videoDirection()
+        {
+            return mMediaDirection[checkBoxSendVideo.Checked][checkBoxReceiveVideo.Checked];
+        }
+        
+        private void buttonHangUp_Click(object sender, EventArgs e)
+        {
+            mController.OnHangupCall();
+            buttonDial.Enabled = true;
+            buttonHangUp.Enabled = false;
+            // These below are needed for the case where somebody dials
+            // this client then hangs up before we can answer or reject
+            // the call.
+            buttonAnswer.Enabled = false;
+            buttonReject.Enabled = false;
+            checkBoxReceiveAudio.Enabled = true;
+            checkBoxReceiveVideo.Enabled = true;
+            checkBoxSendAudio.Enabled = true;
+            checkBoxSendVideo.Enabled = true;
+        }
+
+        private void buttonDial_Click(object sender, EventArgs e)
+        {
+            mController.OnDialNumber(textBoxPhoneNum.Text, this.Bounds, audioDirection(), videoDirection());
+            buttonDial.Enabled = false;
+            checkBoxReceiveAudio.Enabled = false;
+            checkBoxReceiveVideo.Enabled = false;
+            checkBoxSendAudio.Enabled = false;
+            checkBoxSendVideo.Enabled = false;
+        }
+
+        public void SetController(DiallerController dc)
+        {
+            mController = dc;
+        }
+
+        public void SetLoggedIn(bool loggedIn)
+        {
+            // Ensure update occurs on the UI thread!
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<bool>(SetLoggedIn), loggedIn);
+            }
+            else
+            {
+                buttonLogout.Enabled = loggedIn;
+                buttonLogin.Enabled = !loggedIn;
+                buttonDial.Enabled = loggedIn;
+                buttonHangUp.Enabled = false;
+                buttonAnswer.Enabled = false;
+                buttonReject.Enabled = false;
+                buttonAedDemo.Enabled = loggedIn;
+            }
+        }
+
+        public void SetCallActive()
+        {
+            // Ensure update occurs on the UI thread!
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action(SetCallActive));
+            }
+            else
+            {
+                buttonDial.Enabled = false;
+                buttonHangUp.Enabled = true;
+                buttonAnswer.Enabled = false;
+                buttonReject.Enabled = false;
+                checkBoxReceiveAudio.Enabled = false;
+                checkBoxReceiveVideo.Enabled = false;
+                checkBoxSendAudio.Enabled = false;
+                checkBoxSendVideo.Enabled = false;
+            }
+        }
+
+        public void SetCallInactive()
+        {
+            // Ensure update occurs on the UI thread!
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action(SetCallInactive));
+            }
+            else
+            {
+                buttonDial.Enabled = true;
+                buttonHangUp.Enabled = false;
+                buttonAnswer.Enabled = false;
+                buttonReject.Enabled = false;
+                checkBoxReceiveAudio.Enabled = true;
+                checkBoxReceiveVideo.Enabled = true;
+                checkBoxSendAudio.Enabled = true;
+                checkBoxSendVideo.Enabled = true;
+                // Make sure the call window is also closed.
+                mController.OnHangupCall();
+            }
+        }
+
+        public void SetPhoneRinging()
+        {
+            // Ensure update occurs on the UI thread!
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action(SetPhoneRinging));
+            }
+            else
+            {
+                buttonDial.Enabled = false;
+                buttonHangUp.Enabled = false;
+                buttonAnswer.Enabled = true;
+                buttonReject.Enabled = true;
+            }
+        }
+
+        public void AddLogMessage(string logMessage)
+        {
+            // Ensure update occurs on the UI thread!
+            if (listBoxActivity.InvokeRequired)
+            {
+                this.Invoke(new Action<string>(AddLogMessage), logMessage);
+            }
+            else
+            {
+                listBoxActivity.Items.Add(logMessage);
+            }
+        }
+
+        public void OnRemoteHangupCall()
+        {
+            // This signal has probably come from a non-UI thread. We need to make sure
+            // the hang-up request happens on the UI thread.
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<object, EventArgs>(buttonHangUp_Click), this, null);
+            }
+            else
+            {
+                buttonHangUp_Click(this, null);
+            }
+        }
+
+        private void buttonAedDemo_Click(object sender, EventArgs e)
+        {
+            mController.StartAEDDemo();
+        }
+
+        public void EnableHangupCapability()
+        {
+            // This signal has probably come from a non-UI thread. We need to make sure
+            // the hang-up request happens on the UI thread.
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action(EnableHangupCapability));
+            }
+            else
+            {
+                buttonHangUp.Enabled = true;
+            }
+        }
+
+        public void SetSDKVersion(string sdkVersion)
+        {
+            labelSDKVersion.Text = "SDK Version " + sdkVersion;
+        }
+
+    }
+}
---
+++ c#_example/InCallController.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+using FCSDKCLR;
+
+namespace WebRTCClientWinManaged
+{
+    public interface IInCallWindowListener
+    {
+        void OnInCallWindowDestroyed();
+    }
+
+    public class InCallController : IDisposable, ICLI_CallListener, IInCallWindowListener
+    {
+        private CLI_Call mCall;
+        private DiallerController mDiallerController;
+        private bool mDisposed;
+
+        public InCallController(CLI_Call call, DiallerController diallerController)
+        {
+            mCall = call;
+            mCall.SetListener(this);
+            mDiallerController = diallerController;
+        }
+
+        // Public implementation of Dispose pattern callable by consumers. 
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (mDisposed) return;
+
+            if (disposing)
+            {
+                // Release pipes
+                if (mRemotePipeServer != null)
+                {
+                    mRemotePipeServer.Stop();
+                    mRemotePipeServer.Dispose();
+                    mRemotePipeServer = null;
+                }
+
+                if (mLocalPipeServer != null)
+                {
+                    mLocalPipeServer.Stop();
+                    mLocalPipeServer.Dispose();
+                    mLocalPipeServer = null;
+                }
+
+                // Dispose of the video window.
+                if (mCallWnd != null)
+                {
+                    mCallWnd.Dispose();
+                    mCallWnd = null;
+                }
+            }
+            // Free unmanaged objects here
+            if (mCall != null)
+            {
+                mCall.Dispose();
+                mCall = null;
+            }
+
+            mDisposed = true;
+        }
+
+        /// <summary>
+        /// Calculate the desired position and size of the call window.
+        /// </summary>
+        /// <param name="diallerRect">Position and size of the dialler window. (Currently ignored.)</param>
+        /// <returns>A Rectangle object containing the desired coordinates of the call window.</returns>
+        public Rectangle CalculateCallWindowRect(Rectangle diallerRect)
+        {
+            if (mDisposed) throw new ObjectDisposedException(GetType().FullName);
+            // Find the resolution of the primary screen.
+            Rectangle screenBounds = System.Windows.Forms.Screen.PrimaryScreen.Bounds;
+            // We now need to calculate a base size for the window we will display.
+            // We want the area to be approx 1/4 of the screen size.
+            // The aspect ratio needs to be 4:3.
+            // The area of the screen is of course given by width * height.
+            // To work out the width and height of the window:
+            // width * height = area
+            // width = 4 * height / 3
+            // therefore (4 * height / 3) * height = area
+            // height * height = 3 * area / 4
+            // height = sqrt(3*area/4)
+            // and the width is determined from the height.
+            // We want to play with round numbers and also we want the video preview
+            // to have integer width and height (which currently has a quarter of the
+            // width and height of the remote video) so round the height to the nearest
+            // multiple of 12 and then calculate the width.
+            double desiredArea = screenBounds.Width * screenBounds.Height / 4.0;
+            int height = (int)(Math.Round(Math.Sqrt(3.0 * desiredArea / 4.0) / 12.0) * 12.0);
+            int width = height / 3 * 4;
+            Rectangle callWinRect = new Rectangle(
+                screenBounds.Left + (screenBounds.Width - width) / 2,
+                screenBounds.Top + (screenBounds.Height - height) / 2,
+                width,
+                height);
+
+            return callWinRect;
+        }
+
+        private InCallWindow mCallWnd;
+        private VideoViewPipeServer mRemotePipeServer;
+        private VideoViewPipeServer mLocalPipeServer;
+        /*
+        Starts the UI and connects up the named pipes with the similarly named ones for video
+        rendering.
+        Note that the local video does not currently work as no video is being delivered on that
+        pipe, though the pipe server and video window classes are retained.
+        */
+        public void StartUINextToDiallerRect(Rectangle bounds)
+        {
+            if (mDisposed) throw new ObjectDisposedException(GetType().FullName);
+            // Set the names of the remote video pipe in the call in FCSDK           
+            mCall.SetVideoViewName("Remote Video");
+            // Make an InCallWindow for this call, with this as the listener to receive call events
+            mCallWnd = new InCallWindow(this);
+            // Create the actual window as a child of the dialler, but to its right
+            Rectangle callWndRect = CalculateCallWindowRect(bounds);
+            mCallWnd.Show();
+            mCallWnd.DesktopLocation = new Point(callWndRect.Left, callWndRect.Top);
+            mCallWnd.ClientSize = new Size(callWndRect.Width, callWndRect.Height);
+
+            // Set the refresh view function in the VideoWindow (which will set it in its VideoView)
+            mCallWnd.GetLocalVideoWindow().SetRefreshViewFunction(
+                new RedrawWindow(mCallWnd.GetLocalVideoWindow()));
+            mCallWnd.GetRemoteVideoWindow().SetRefreshViewFunction(
+                new RedrawWindow(mCallWnd.GetRemoteVideoWindow()));
+            // Create pipe servers for video windows
+            mCallWnd.GetRemoteVideoWindow().WindowName = "Remote Video";
+            mRemotePipeServer = new VideoViewPipeServer("Remote Video", mCallWnd.GetRemoteVideoWindow());
+            mRemotePipeServer.Start();
+            mCallWnd.GetLocalVideoWindow().WindowName = "Local Video";
+            mLocalPipeServer = new VideoViewPipeServer("Local Video", mCallWnd.GetLocalVideoWindow());
+            mLocalPipeServer.Start();
+        }
+
+        class RedrawWindow : ICLI_ViewRefresher
+        {
+            private VideoWindow mCallWnd;
+
+            public RedrawWindow(VideoWindow callWnd)
+            {
+                mCallWnd = callWnd;
+            }
+
+            public void RefreshViewFunc()
+            {
+                mCallWnd.Invalidate();
+            }
+        }
+
+        public void OnCallFailed()
+        {
+            // Just report it.
+            mDiallerController.LogEvent("InCallController.OnCallFailed called");
+        }
+
+        public void OnDialFailed()
+        {
+            // Just report it.
+            mDiallerController.LogEvent("InCallController.OnDialFailed called");
+        }
+
+        public void OnInboundQualityChange(int inboundQuality)
+        {
+            // Just report it.
+            mDiallerController.LogEvent("Inbound call quality changed to " + inboundQuality);
+        }
+
+        public void OnLocalMediaStream()
+        {
+            // Just report it.
+            mDiallerController.LogEvent("InCallController.OnLocalMediaStream called");
+        }
+
+        public void OnRemoteMediaStream()
+        {
+            // Just report it.
+            mDiallerController.LogEvent("InCallController.OnRemoteMediaStream called");
+        }
+
+        public void OnRemoteDisplayNameChanged(String newName)
+        {
+            // Just report it.
+            mDiallerController.LogEvent("OnRemoteDisplayNameChanged called");
+            mDiallerController.LogEvent("New name: " + newName);
+        }
+
+        public void OnRemoteHeld()
+        {
+            // Just report it.
+            mDiallerController.LogEvent("OnRemoteHeld called");
+        }
+
+        public void OnRemoteUnheld()
+        {
+            // Just report it.
+            mDiallerController.LogEvent("OnRemoteUnheld called");
+        }
+
+        public void OnOutboundAudioFailure()
+        {
+            // Just report it.
+            mDiallerController.LogEvent("InCallController.OnOutboundAudioFailure called");
+        }
+
+        public void OnOutboundVideoFailure()
+        {
+            // Just report it.
+            mDiallerController.LogEvent("InCallController.OnOutboundVideoFailure called");
+        }
+
+        public void OnStatusChanged(CLI_CallStatus newStatus)
+        {
+            if (mDisposed)
+            {
+                // Just report it.
+                mDiallerController.LogEvent("InCallController.OnStatusChange called after object disposed");
+                mDiallerController.LogEvent("Status was " + newStatus);
+                return;
+            }
+            switch (newStatus)
+            {
+            case CLI_CallStatus.UNINITIALIZED:
+                mDiallerController.LogEvent("status change UNINITIALIZED");
+                break;
+            case CLI_CallStatus.SETUP:
+                mDiallerController.LogEvent("status change SETUP");
+                break;
+            case CLI_CallStatus.ALERTING:
+                mDiallerController.LogEvent("status change ALERTING");
+                break;
+            case CLI_CallStatus.RINGING:
+                mDiallerController.LogEvent("status change RINGING");
+                // We report the event to the dialler controller so that hang-up function can be enabled.
+                mDiallerController.OnEntryToRingingState();
+                break;
+            case CLI_CallStatus.MEDIA_PENDING:
+                mDiallerController.LogEvent("status change MEDIA_PENDING");
+                break;
+            case CLI_CallStatus.IN_CALL:
+                mDiallerController.LogEvent("status change IN_CALL");
+                // We report the event to the dialler controller so that hang-up function can be enabled.
+                mDiallerController.OnEntryToInCallState();
+                break;
+            case CLI_CallStatus.TIMED_OUT:
+                mDiallerController.LogEvent("status change TIMED_OUT");
+                break;
+            case CLI_CallStatus.BUSY:
+                mDiallerController.LogEvent("status change BUSY");
+                mDiallerController.OnCallStatusBusy();
+                break;
+            case CLI_CallStatus.NOT_FOUND:
+                mDiallerController.LogEvent("status change NOT_FOUND");
+                break;
+            case CLI_CallStatus.CALL_ERROR:
+                mDiallerController.LogEvent("status change CALL_ERROR");
+                break;
+            case CLI_CallStatus.ENDED:
+                mDiallerController.LogEvent("status change ENDED");
+                mDiallerController.OnRemoteHangupCall();
+                break;
+            default:
+                mDiallerController.LogEvent("status change **UNRECOGNISED**");
+                break;
+            }
+        }
+
+        public void OnInCallWindowDestroyed()
+        {
+            // Report it.
+            mDiallerController.LogEvent("InCallController.OnInCallWindowDestroyed called");
+            if (mDisposed)
+            {
+                mDiallerController.LogEvent("InCallController has been disposed");
+                return;
+            }
+            // Hangup the call.
+            mCall.End();
+            // Notify DiallerController that call controller is ending.
+            mDiallerController.OnCallControllerEnded();
+        }
+
+        public void OnHangupCall()
+        {
+            // Report it.
+            mDiallerController.LogEvent("InCallController.OnHangupCall called");
+            if (mDisposed)
+            {
+                mDiallerController.LogEvent("InCallController has been disposed");
+                return;
+            }
+            // Hangup the call.
+            mCall.End();
+            // Notify DiallerController that call controller is ending.
+            mDiallerController.OnCallControllerEnded();
+        }
+
+        public void OnRejectCallPressed()
+        {
+            // Report it.
+            mDiallerController.LogEvent("InCallController.OnRejectCallPressed called");
+            if (mDisposed)
+            {
+                mDiallerController.LogEvent("InCallController has been disposed");
+                return;
+            }
+            // Hangup the call.
+            mCall.End();
+            // Notify DiallerController that call controller is ending.
+            mDiallerController.OnCallControllerEnded();
+        }
+
+
+        internal void OnAnswerCallPressed(CLI_MediaDirection audioDirection, CLI_MediaDirection videoDirection)
+        {
+            // Report it.
+            mDiallerController.LogEvent("InCallController.OnAnswerCallPressed called");
+            if (mDisposed)
+            {
+                mDiallerController.LogEvent("InCallController has been disposed");
+                return;
+            }
+            // Answer the call.
+            mCall.Answer(audioDirection, videoDirection);
+        }
+    }
+}
